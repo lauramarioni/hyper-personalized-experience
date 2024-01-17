35726b1e9f1d14454914e21827bdee09c1f4e8e7
lauramarioni <74243685+lauramarioni@users.noreply.github.com>
2024-01-17 13:06:33 +0100

    Hello.py

--- Hello.py
+++ Hello.py
@@ -12,41 +12,72 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import streamlit as st
-from streamlit.logger import get_logger
+# import streamlit as st
+# from streamlit.logger import get_logger
 
-LOGGER = get_logger(__name__)
+# LOGGER = get_logger(__name__)
 
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
+# def run():
+#     st.set_page_config(
+#         page_title="Hello",
+#         page_icon="👋",
+#     )
+
+#     st.write("# Welcome to Streamlit! 👋")
+
+#     st.sidebar.success("Select a demo above.")
+
+#     st.markdown(
+#         """
+#         CIAOOOOOOOO
+#         Streamlit is an open-source app framework built specifically for
+#         Machine Learning and Data Science projects.
+#         **👈 Select a demo from the sidebar** to see some examples
+#         of what Streamlit can do!
+#         ### Want to learn more?
+#         - Check out [streamlit.io](https://streamlit.io)
+#         - Jump into our [documentation](https://docs.streamlit.io)
+#         - Ask a question in our [community
+#           forums](https://discuss.streamlit.io)
+#         ### See more complex demos
+#         - Use a neural net to [analyze the Udacity Self-driving Car Image
+#           Dataset](https://github.com/streamlit/demo-self-driving)
+#         - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+#     """
+#     )
+
+
+# if __name__ == "__main__":
+#     run()
+
+# Second
+import streamlit as st 
+import anthropic
+with st.sidebar:
+    anthropic_api_key = st.text_input("Anthropic API Key", key="file_qa_api_key", type="password")
+    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/1_File_Q%26A.py)"
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+
+st.title("📝 File Q&A with Anthropic") uploaded_file = st.file_uploader("Upload an article", type=("txt", "md")) question = st.text_input(
+    "Ask something about the article",
+    placeholder="Can you give me a short summary?",
+    disabled=not uploaded_file,
+)
+if uploaded_file and question and not anthropic_api_key:
+    st.info("Please add your Anthropic API key to continue.")
+
+if uploaded_file and question and anthropic_api_key:
+    article = uploaded_file.read().decode()
+    prompt = f"""{anthropic.HUMAN_PROMPT} Here's an article:\n\n
+    {article}\n\n\n\n{question}{anthropic.AI_PROMPT}"""
+
+    client = anthropic.Client(api_key=anthropic_api_key)
+    response = client.completions.create(
+        prompt=prompt,
+        stop_sequences=[anthropic.HUMAN_PROMPT],
+        model="claude-v1", #"claude-2" for Claude 2 model
+        max_tokens_to_sample=100,
     )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        CIAOOOOOOOO
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
-
-
-if __name__ == "__main__":
-    run()
+    st.write("### Answer")
+    st.write(response.completion)
